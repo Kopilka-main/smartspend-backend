5282de13c3a9291bf666579d3fccc857ce241450
GoncharovIY <goncharoviy@control.mos.ru>
2026-04-24 18:04:15 +0300

    fix

--- src/app/models/user_finance.py
+++ src/app/models/user_finance.py
@@ -3,7 +3,7 @@ from datetime import datetime
 from decimal import Decimal
 
 from sqlalchemy import BigInteger, DateTime, ForeignKey, Integer, Numeric, func
-from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.dialects.postgresql import JSONB, UUID
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from src.app.core.database import Base
